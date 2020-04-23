fcfe40372c429835b79e79e80daba68230e77c2d
Dyskal <yoshilexis@gmail.com>
2020-04-23 20:47:17 +0200

    add sense joystick reload

--- Main.py
+++ Main.py
@@ -5,26 +5,28 @@ from os import path, getcwd
 from Excel import createXlsx, updateXslx
 # from sense_hat import SenseHat
 
-main = ThemedTk(background=True, theme="equilux")    # On crée une fenetre tkinter
-if not path.exists(getcwd() + '\\python_weather_data.xlsx'):    # On vérifie si le fichier excel existe
-    createXlsx()                                                # Sinon on le crée
+main = ThemedTk(background=True, theme="equilux")  # On crée une fenetre tkinter
+if not path.exists(getcwd() + '\\python_weather_data.xlsx'):  # On vérifie si le fichier excel existe
+    createXlsx()  # Sinon on le crée
 
 # sense = SenseHat()
-timesv = StringVar()        # On définit des chaines de caractère variables pour les labels tkinter
+timesv = StringVar()  # On définit des chaines de caractère variables pour les labels tkinter
 tempsv = StringVar()
 pressuresv = StringVar()
 humiditysv = StringVar()
 
 
-def reloadData():       # Cette fonction permet de mettre à jour les données météo
+def reloadData():  # Cette fonction permet de mettre à jour les données météo
     from Data import getData
     data = getData()
     timesv.set("Date: " + data["Time"])
     tempsv.set("Température: " + data["Temperature"] + " °C")
     pressuresv.set("Pression: " + data["Pressure"] + " hPa")
     humiditysv.set("Humidité: " + data["Humidity"] + " %")
-    updateXslx()        # Et ensuite de mettre les valeurs dans le fichier Excel
+    updateXslx()  # Et ensuite de mettre les valeurs dans le fichier Excel
 
+
+# sense.stick.direction_any = reloadData
 
 main.title('Station météo')
 main.geometry('350x75')
@@ -32,16 +34,16 @@ main.iconphoto(True, PhotoImage(file="resources/icon.png"))
 style = ThemedStyle(main)
 reloadData()
 
-Time = Label(main, textvariable=timesv)     # On crée un label qui affiche la date
+Time = Label(main, textvariable=timesv)  # On crée un label qui affiche la date
 Time.grid(row=0, column=0, columnspan=3)
 
-Temperature = Label(main, textvariable=tempsv)      # On crée un label qui affiche la température
+Temperature = Label(main, textvariable=tempsv)  # On crée un label qui affiche la température
 Temperature.grid(row=1, column=0)
 
-Pressure = Label(main, textvariable=pressuresv)     # On crée un label qui affiche la pression
+Pressure = Label(main, textvariable=pressuresv)  # On crée un label qui affiche la pression
 Pressure.grid(row=1, column=1)
 
-Humidity = Label(main, textvariable=humiditysv)     # On crée un label qui affiche l'humidité
+Humidity = Label(main, textvariable=humiditysv)  # On crée un label qui affiche l'humidité
 Humidity.grid(row=1, column=2)
 
 Reload = Button(main, text="Recharger les données", command=reloadData)
@@ -49,3 +51,6 @@ Reload = Button(main, text="Recharger les données", command=reloadData)
 Reload.grid(row=2, column=0, columnspan=3)
 
 main.mainloop()
+
+while True:
+    pass
