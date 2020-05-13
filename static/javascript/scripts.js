63ed63d4eee6f10e2898011661d1d22a296ccf8d
trlseysh <trlseysh@gmail.com>
2020-05-13 16:01:48 +0200

    Page Web, globalement

---
+++ static/javascript/scripts.js
@@ -0,0 +1,3 @@
+function ReloadPage() {
+    location.reload();
+}
--- web/views.py
+++ web/views.py
@@ -8,5 +8,5 @@ def main(request):
     out = Popen([executable, dirname(dirname(__file__))+"/gui/getdata.py"], shell=False, stdout=PIPE, stderr=STDOUT,
                 universal_newlines=True).communicate()[0].strip()
     result = out.split("\n", 3)
-    return render(request, "main.html",
+    return render(request, "homepage.html",
                   {'time': result[0], 'temp': result[1], 'pressure': result[2], 'humidity': result[3]})
