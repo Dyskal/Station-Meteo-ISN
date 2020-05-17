3db400f17f347784d13561dbdeda240f5d2c21b6
Dyskal <yoshilexis@gmail.com>
2020-05-17 21:38:15 +0200

    update project

--- manage.py
+++ manage.py
@@ -7,8 +7,6 @@ def main():
     environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
     try:
         from django.core.management import execute_from_command_line
-        from django.core.management.commands.runserver import Command
-        Command.default_port = "25565"
     except ImportError:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
